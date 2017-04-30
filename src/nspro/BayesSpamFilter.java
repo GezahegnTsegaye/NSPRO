0c1ae625064e60589270dbef5136d7448d58a753
lemma <lemma@gezu>
2017-04-30 02:18:54 +0200

    including the user interface to insert the spamtest, spamvalidation, spam training, and also for ham messages

--- src/nspro/BayesSpamFilter.java
+++ src/nspro/BayesSpamFilter.java
@@ -5,11 +5,109 @@
  */
 package nspro;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * This class filter the spam based bayes algorithm 
+ * This class filter the spam based bayes algorithm
+ *
  * @author Gezahegn
  */
 public class BayesSpamFilter {
-    
-    
+
+    Map<String, Double> spam;
+    Map<String, Double> ham;
+    int spamFeedings = 0;
+    int hamFeedings = 0;
+    private final static double epsilon = 0.05;
+    private double spamThreshhold = 0.5;
+
+    private void feed(String content, boolean isSpam) {
+        Map<String, Double> map = isSpam ? spam : ham;
+
+        for (String word : content.split("\\s+")) {
+            Double val = map.containsKey(word) ? map.get(word) : 0.;
+            map.put(word, ++val);
+        }
+    }
+
+    /**
+     * make the listed word sequential
+     *
+     * @param map
+     * @param size
+     */
+    private void normalize(Map<String, Double> map, int size) {
+        if (size == 0) {
+            size = 1;
+        }
+        for (String word : map.keySet()) {
+            map.put(word, map.get(word) / size);
+        }
+    }
+
+    private void fillMap(Map<String, Double> map, Map<String, Double> otherMap) {
+
+        for (String word : otherMap.keySet()) {
+            if (!map.containsKey(word)) {
+                map.put(word, epsilon);
+            }
+        }
+    }
+
+    public double isSpamProbability(String content) {
+        String[] words = content.split("\\s+");
+
+        double result = 1d;
+
+        for (String word : words) {
+            if (spam.containsKey(word)) {
+                result *= ham.get(word) / spam.get(word);
+            }
+        }
+
+        return 1d / (1d + result);
+    }
+
+    public boolean isSpam(String content) {
+        return isSpamProbability(content) > spamThreshhold;
+    }
+
+    public double getSpamThreshhold() {
+        return spamThreshhold;
+    }
+
+    public void setSpamThreshhold(double spamThreshhold) {
+        this.spamThreshhold = spamThreshhold;
+    }
+
+    public BayesSpamFilter(String[] trainingSpam, String[] trainingHam) {
+        spam = new HashMap<>();
+        ham = new HashMap<>();
+
+        for (String spam : trainingSpam) {
+            feed(spam, true);
+        }
+
+        for (String ham : trainingHam) {
+            feed(ham, false);
+        }
+
+        spamFeedings = trainingSpam.length;
+        hamFeedings = trainingHam.length;
+
+        int min = Math.min(spamFeedings, hamFeedings);
+
+        if (min != 0) {
+            spamFeedings /= min;
+            hamFeedings /= min;
+        }
+
+        fillMap(spam, ham);
+        fillMap(ham, spam);
+
+        normalize(spam, spamFeedings);
+        normalize(ham, hamFeedings);
+    }
+
 }
---
+++ src/nspro/TrainWithDataset.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nspro;
+
+/**
+ *
+ * @author Gezahegn
+ */
+public class TrainWithDataset {
+
+    private BayesSpamFilter spamfilter;
+    private String[] validationSpam;
+    private String[] validationHam;
+
+    public TrainWithDataset(BayesSpamFilter spamfilter, String[] validationSpam, String[] validationHam) {
+        this.spamfilter = spamfilter;
+        this.validationSpam = validationSpam;
+        this.validationHam = validationHam;
+    }
+    
+    /**
+     * calculate the best threshold values for spam validation 
+     * and ham validation
+     * @param spam
+     * @param ham
+     * @param spamThreshold
+     * @return 
+     */
+
+    public Performance calculatePerformance(String[] spam, String[] ham, double spamThreshold) {
+        double prevThreshold = spamfilter.getSpamThreshhold();
+        spamfilter.setSpamThreshhold(spamThreshold);
+
+        Performance per = new Performance();
+        int tp = 0;
+        int fp = 0;
+        int fn = 0;
+
+        for (String s : spam) {
+            if (spamfilter.isSpam(s)) {
+                tp++;
+            } else {
+                fn++;
+            }
+        }
+
+        for (String h : ham) {
+            if (spamfilter.isSpam(h)) {
+                fp++;
+            }
+        }
+        per.setPrecision((double) tp / (tp + fp));
+        per.setRecall((double) tp / (tp + fn));
+
+        spamfilter.setSpamThreshhold(prevThreshold);
+        return per;
+    }
+
+    /**
+     *
+     * @param testSpam
+     * @param testHam
+     * @param iterations
+     * @return
+     */
+    public Performance train(String[] testSpam, String[] testHam, int iterations) {
+        iterations++;
+
+        Performance max = null;
+        double bestThreshold = 0;
+        for (double i = 1; i < iterations; i++) {
+            Performance perf = calculatePerformance(validationSpam, validationHam, i/iterations);
+            
+            if(max == null || max.getF1Score() < perf.getF1Score()){
+                max = perf;
+                bestThreshold = i/iterations;
+                
+            }
+        }
+        
+        getSpamFilter().setSpamThreshhold(bestThreshold);
+        
+        return calculatePerformance(testHam, testHam, bestThreshold);
+    }
+    
+    
+
+   private BayesSpamFilter getSpamFilter(){
+       return spamfilter;
+   }
+
+}
